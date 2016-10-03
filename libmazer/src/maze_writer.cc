e6fd445a135e6141228bb4c2ee027d4bbd0cf107
Nicholas Amor <nick.amor@gmail.com>
2016-10-03 23:27:10 +1100

    Clean line length

--- libmazer/src/maze_writer.cc
+++ libmazer/src/maze_writer.cc
@@ -18,7 +18,9 @@ MazeWriter::Write(std::shared_ptr<Maze> maze)
     std::ofstream stream(filename, std::ios::binary);
 
     auto &&edges = maze->GetEdges();
-    int width = maze->GetWidth(), height = maze->GetHeight(), num_edges = static_cast<int>(edges.size());
+    int width = maze->GetWidth();
+    int height = maze->GetHeight();
+    int num_edges = static_cast<int>(edges.size());
 
     stream.write(reinterpret_cast<char *>(&width), sizeof width);
     stream.write(reinterpret_cast<char *>(&height), sizeof height);
@@ -26,10 +28,10 @@ MazeWriter::Write(std::shared_ptr<Maze> maze)
 
     for (auto &edge : edges)
     {
-        stream.write(reinterpret_cast<const char *>(&edge.src.x), sizeof edge.src.x);
-        stream.write(reinterpret_cast<const char *>(&edge.src.y), sizeof edge.src.y);
-        stream.write(reinterpret_cast<const char *>(&edge.dst.x), sizeof edge.dst.x);
-        stream.write(reinterpret_cast<const char *>(&edge.dst.y), sizeof edge.dst.y);
+        stream.write((const char *)(&edge.src.x), sizeof edge.src.x);
+        stream.write((const char *)(&edge.src.y), sizeof edge.src.y);
+        stream.write((const char *)(&edge.dst.x), sizeof edge.dst.x);
+        stream.write((const char *)(&edge.dst.y), sizeof edge.dst.y);
     }
 
 }
